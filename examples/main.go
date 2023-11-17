7fccecf1a63a0a1470106877dae6b0199d661dc4
shibin <shibin@zhihu.com>
2023-11-17 18:23:33 +0800

    clean silly code

--- examples/main.go
+++ examples/main.go
@@ -8,23 +8,21 @@ import (
 	"github.com/alingse/ctxcache"
 )
 
-var funcID = "get_number"
-
 func getNumber(n int64) string {
 	fmt.Printf("got number: %d \n", n)
 	return strconv.FormatInt(n, 10)
 }
 
 func callGetNumber(ctx context.Context, n int64) {
-	getNumberCache, _ := ctxcache.FromContext[int64, string](ctx, funcID)
-	getNumberCache(n)
+	getNumber2, _ := ctxcache.FromContext[int64, string](ctx, ctxcache.FuncID("getNumber"), getNumber)
+	getNumber2(n)
 }
 
 func main() {
 	var ctx = context.Background()
 
 	// register funcID' cache
-	ctx = ctxcache.WithCache[int64, string](ctx, funcID, getNumber)
+	ctx = ctxcache.WithCache[int64, string](ctx, ctxcache.FuncID("getNumber"), getNumber)
 
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
